42ad09ce7fbc3a3d9bc928fbfeb466362998ae28
Gasil <598863539@qq.com>
2019-01-17 16:56:29 +0800

    No.4

---
+++ longestPalindromicSubstring.php
@@ -0,0 +1,10 @@
+<?php
+    
+
+
+
+class Solution {
+    function longestPalindrome($s) {
+        
+    }
+}
---
+++ medianOfTwoSortedArrays.php
@@ -0,0 +1,23 @@
+<?php
+
+$nums1 = [1,3];
+$nums2 = [2];
+
+$solution = new solution();
+$res = $solution->findMedianSortedArrays1($nums1, $nums2);
+var_dump($res);
+
+class solution{
+
+    public function findMedianSortedArrays1($num1, $num2){  
+        $nums = array_merge($num1, $num2);
+        sort($nums);
+        $length = sizeof($nums);
+        if($length % 2 == 0){
+            return ($nums[$length/2] + $nums[$length/2 -1])/2;
+        }else{
+            return $nums[($length-1)/2];
+        }
+    }
+
+}
