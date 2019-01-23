94c9ca57878e1b423a2a1bd7359791db5a1a6770
Gasil <598863539@qq.com>
2019-01-23 18:27:08 +0800

    No.11

--- php/containerWithMostWater.php
+++ php/containerWithMostWater.php
@@ -17,4 +17,26 @@ class Solution {
         }
         return $max;
     }
+
+    //从两边向中间缩
+    //每次保留较长一边，另一边向内一步
+    //比较移动前后的面积
+    function maxArea2($height){
+        $minIndex = 0;
+        $maxIndex = sizeof($height)-1;
+        $max = min($height[$minIndex],$height[$maxIndex]) * (sizeof($height)-1);
+
+        while($minIndex < $maxIndex){
+            if($height[$minIndex] < $height[$maxIndex]){
+                $minIndex++;
+            }else{
+                $maxIndex--;
+            }
+            $nextArea = min($height[$minIndex],$height[$maxIndex]) * ($maxIndex - $minIndex);
+            if($nextArea > $max){
+                $max = $nextArea;
+            }
+        }
+        return $max;
+    }
 }
