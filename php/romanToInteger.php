fa5b8248d2836591b24998d5250c56f607b18662
Gasil <598863539@qq.com>
2019-02-12 15:45:18 +0800

    No.13

--- php/romanToInteger.php
+++ php/romanToInteger.php
@@ -2,10 +2,10 @@
 $s = 'XXX';
 
 $solution = new Solution();
-$res = $solution->romanToInt($s);
+$res = $solution->romanToInt1($s);
 
 class Solution {
-    function romanToInt($s) {
+    function romanToInt1($s) {
         $value = [
             'I'=>1,'V'=>5,'X'=>10,'L'=>50,'C'=>100,'D'=>500,'M'=>1000
         ];
@@ -21,6 +21,32 @@ class Solution {
             }
         }
         $sum += $value[$s[$length-1]];
+        return $sum;
+    }
+
+    function romanToInt2($s) {
+        $value = [
+            'I'=>1,'V'=>5,'X'=>10,'L'=>50,'C'=>100,'D'=>500,'M'=>1000
+        ];
+
+        $sum = 0;
+        $length = strlen($s);
+
+        for ($i = 0; $i<$length; $i++){
+            $sum += $value[$s[$i]];
+
+            if($i != 0){
+                if(($s[$i] == 'V' || $s[$i] == 'X') && $s[$i-1] == 'I'){
+                    $less += 1;
+                }elseif(($s[$i] == 'L' || $s[$i] == 'C') && $s[$i-1] == 'X'){
+                    $less += 10;
+                }elseif(($s[$i] == 'D' || $s[$i] == 'M') && $s[$i-1] == 'C'){
+                    $less += 100;
+                }
+            }
+        }
+        $sum = $sum-$less*2;
+        
         return $sum;
     }
 }
