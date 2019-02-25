e051dcb33edddba2cb47ead55486bde216afbcfe
Gasil <598863539@qq.com>
2019-02-25 17:24:04 +0800

    No.15

--- php/threeSum.php
+++ php/threeSum.php
@@ -4,9 +4,9 @@ class Solution {
     function threeSum($nums) {
         $arr = [];
         foreach ($nums as $k => $v){
-            for($i = 0; $i<sizeof($nums); $i++){
+            for($i = 0; $i<sizeof($nums, 0); $i++){
                 if($i == $k)continue;
-                for ($j = $i+1; $j<sizeof($nums); $j++){
+                for ($j = $i+1; $j<sizeof($nums, 0); $j++){
                     if($j == $k)continue;
                     if($nums[$i] + $nums[$j] == -$v){
                         $temarr = [$nums[$i],$nums[$j],$v];
@@ -14,6 +14,26 @@ class Solution {
                         $arr[] = $temarr;
                     }
                 }
+            }
+        }
+        return array_unique($arr, SORT_REGULAR);
+    }
+
+    //还超。。
+    function threeSum2($nums){
+        $arr = [];
+        for ($i = 0; $i<= sizeof($nums, 0) - 2; $i++){
+            $min = $i+1;
+            $max = sizeof($nums, 0)-1;
+            while($min < $max){
+                if($nums[$i] + $nums[$min] + $nums[$max] >0){
+                    $max --;
+                }elseif ($nums[$i] + $nums[$min] + $nums[$max] <0){
+                    $min ++;
+                }else{
+                    $arr[] = [$nums[$i] , $nums[$min], $nums[$max]];
+                    $max --;
+                    $min ++;                }
             }
         }
         return array_unique($arr, SORT_REGULAR);
