48885ef44b83e147ea7c1a3601a92fcda33ff363
Gasil7 <598863539@qq.com>
2019-03-08 01:35:53 +0900

    No.22

---
+++ php/generateParentheses.php
@@ -0,0 +1,29 @@
+<?php
+class Solution {
+    public static $arr = [];
+
+    function generateParenthesis($n){
+        $this->abc($n, $left=0, $right=0, $str);
+        return self::$arr;
+    }
+
+    function abc($n, $left, $right, $str){
+        if(strlen($str) == $n*2){
+            self::$arr[] = $str;
+            return;
+        }
+
+        if($left < $n){
+            $this->abc($n, $left+1, $right, $str.'(');
+        }
+
+        if($left > $right){
+            $this->abc($n, $left, $right+1, $str.')');
+        }
+    }
+}
+
+
+$solution = new Solution();
+$res = $solution->generateParenthesis(3);
+var_dump($res);
