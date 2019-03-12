479d0108688a3b29ef48b62d06350e3cd7ce629f
Gasil <598863539@qq.com>
2019-03-12 14:49:50 +0800

    No.27

---
+++ php/removeElement.php
@@ -0,0 +1,25 @@
+<?php   
+    class Solution {
+
+        /**
+         * @param Integer[] $nums
+         * @param Integer $val
+         * @return Integer
+         */
+        function removeElement(&$nums, $val) {
+            $length = sizeof($nums);
+            for ($i=0; $i < $length; $i++) { 
+                if($nums[$i] == $val){
+                    unset($nums[$i]);
+                }
+            }
+            return sizeof($nums);
+        }
+    }
+
+$solution = new Solution();
+$nums = [0,0,1,1,1,2,2,3,3,4];
+$val = 4;
+
+$res = $solution->removeElement($nums, $val);
+var_dump($res);
